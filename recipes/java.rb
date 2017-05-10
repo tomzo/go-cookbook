4751d947df71713a0a6db81545d2c41adde8dd57
Tomasz Setkowski <tom@ai-traders.com>
2017-05-10 12:55:32 +0000

    fix java to install 8

--- recipes/java.rb
+++ recipes/java.rb
@@ -15,5 +15,5 @@
 ##########################################################################
 
 # must be early so that java home attrbute gets assigned soon enough
-node.default!['java']['jdk_version'] = '8'
+node.force_default['java']['jdk_version'] = '8'
 include_recipe 'java'
